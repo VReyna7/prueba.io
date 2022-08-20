d30ef7f161bd7241f5a59336190cdad3adc29faf
Juan <89045210+VReyna7@users.noreply.github.com>
2022-08-20 20:46:45 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal();
+    sr.reveal(".navbar",{
+        duration: 3000,
+        origin: "bottom",
+        distance: "-100px"
+    });
+
+    sr.reveal(".objetivos",{
+        duration: 5000,
+        origin: "bottom",
+        distance: "-100px"
+    });
