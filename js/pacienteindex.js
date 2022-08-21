80bb97caa3223c79ffbf0fb9b3071d7bd205fb33
Juan <89045210+VReyna7@users.noreply.github.com>
2022-08-21 19:43:32 +0200

    Add files via upload

---
+++ js/pacienteindex.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+
+    sr.reveal(".navbar",{
+        duration: 1000,
+        origin: "bottom",
+        distance: "-100px"
+    });
+
+    sr.reveal(".footer",{
+        duration: 2000,
+        origin: "top",
+        distance: "-200px"
+    });
+
+    sr.reveal(".info",{
+        duration: 2000,
+        origin: "left",
+        distance: "200px"
+    });
+
+    sr.reveal(".info2",{
+        duration: 2000,
+        origin: "left",
+        distance: "200px"
+    });
