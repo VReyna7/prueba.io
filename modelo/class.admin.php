51e1b1b51c853f214b86ea506e8525e64841a42b
Juan <89045210+VReyna7@users.noreply.github.com>
2022-08-30 11:57:40 -0600

    Add files via upload

--- modelo/class.admin.php
+++ modelo/class.admin.php
@@ -1,102 +1,110 @@
 <?php
-class Admin{
+class Admin
+{
     private $id;
     private $nombre;
     private $apellido;
     private $pass;
     private $correo;
     private $sexo;
+    private $foto;
     private $fechaNac;
 
-    public function veriData($nombre,$apellido,$pass,$correo,$sexo,$fechaNac){
-        if(isset($nombre) && isset($apellido) && isset($pass) && isset($correo) && isset($sexo) && isset($fechaNac)){
+    public function veriData($nombre, $apellido, $pass, $correo, $sexo, $fechaNac)
+    {
+        if (isset($nombre) && isset($apellido) && isset($pass) && isset($correo) && isset($sexo) && isset($fechaNac)) {
             $this->nombre = $nombre;
             $this->apellido = $apellido;
             $this->pass = $pass;
             $this->correo = $correo;
             $this->sexo = $sexo;
             $this->fechaNac = $fechaNac;
-        }else{
+        } else {
             throw new Exception('Error, Tiene que rellenar todos los datos');
         }
     }
 
-    public function newAdmin(){
+    public function newAdmin()
+    {
         $dbh = new Conexion();
         $conexion = $dbh->get_conexion();
         $sql = "insert into admin (nombre, apellido, pass, correo, sexo, fecha_nac) values (:nombre, :apellido, md5(:pass), :correo, :sexo, :fecha_nac)";
-        $stmt = $conexion->prepare($sql) ;
-        $stmt->bindParam(":nombre",$this->nombre);
-        $stmt->bindParam(":apellido",$this->apellido);
-        $stmt->bindParam(":pass",$this->pass);
-        $stmt->bindParam(":correo",$this->correo);
-        $stmt->bindParam(":sexo",$this->sexo);
-        $stmt->bindParam(":fecha_nac",$this->fechaNac);
-        if(!$stmt){
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":apellido", $this->apellido);
+        $stmt->bindParam(":pass", $this->pass);
+        $stmt->bindParam(":correo", $this->correo);
+        $stmt->bindParam(":sexo", $this->sexo);
+        $stmt->bindParam(":fecha_nac", $this->fechaNac);
+        if (!$stmt) {
             throw new Exception("Error. No se pudo conectar con la base de datos");
-        }else{
+        } else {
             $stmt->execute();
             //Se a registrado correctamente
         }
     }
 
-    public function newDoctor(){
+    public function newDoctor()
+    {
         $dbh = new Conexion();
         $conexion = $dbh->get_conexion();
         $sql = "insert into admin (nombre, apellido, pass, correo, sexo, fecha_nac) values (:nombre, :apellido, md5(:pass), :correo, :sexo, :fecha_nac)";
-        $stmt = $conexion->prepare($sql) ;
-        $stmt->bindParam(":nombre",$this->nombre);
-        $stmt->bindParam(":apellido",$this->apellido);
-        $stmt->bindParam(":pass",$this->pass);
-        $stmt->bindParam(":correo",$this->correo);
-        $stmt->bindParam(":sexo",$this->sexo);
-        $stmt->bindParam(":fecha_nac",$this->fechaNac);
-        if(!$stmt){
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":apellido", $this->apellido);
+        $stmt->bindParam(":pass", $this->pass);
+        $stmt->bindParam(":correo", $this->correo);
+        $stmt->bindParam(":sexo", $this->sexo);
+        $stmt->bindParam(":fecha_nac", $this->fechaNac);
+        if (!$stmt) {
             throw new Exception("Error. No se pudo conectar con la base de datos");
-        }else{
+        } else {
             $stmt->execute();
             //Se a registrado correctamente
         }
     }
 
-    public function extAdmin($user){
+    public function extAdmin($user)
+    {
         $dbh = new Conexion;
         $conexion = $dbh->get_conexion();
         $sql = 'Select * from admin where correo=:correo';
         $stmt = $conexion->prepare($sql);
-        $stmt->bindParam(":correo",$user);
-        if(!$stmt){
+        $stmt->bindParam(":correo", $user);
+        if (!$stmt) {
             throw new Exception("Error. fallo en la base de datos");
-        }else{
+        } else {
             $stmt->execute();
-            if($stmt->rowCount()){
-                return true; 
-            }else{
+            if ($stmt->rowCount()) {
+                return true;
+            } else {
                 return false;
             }
         }
     }
 
-    public function searchAdmin($user, $pass){
+    public function searchAdmin($user, $pass)
+    {
         $dbh = new Conexion;
         $conexion = $dbh->get_conexion();
         $sql = 'Select * from admin where correo=:correo and pass=:pass';
         $stmt = $conexion->prepare($sql);
-        $stmt->bindParam(":correo",$user);
-        $stmt->bindParam(":pass",$pass);
-        if(!$stmt){
+        $stmt->bindParam(":correo", $user);
+        $stmt->bindParam(":pass", $pass);
+        if (!$stmt) {
             throw new Exception("Error. fallo en la base de datos");
-        }else{
+        } else {
             $stmt->execute();
-            if($stmt->rowCount()){
-                return true; 
-            }else{
+            if ($stmt->rowCount()) {
+                return true;
+            } else {
                 return false;
             }
         }
     }
 
-    public function allAdmins(){
+    public function allAdmins()
+    {
         $dbh = new Conexion;
         $conexion = $dbh->get_conexion();
         $sql = 'select * from admin order by id';
@@ -106,46 +114,94 @@ class Admin{
         return $admins;
     }
 
-
-    public function setAdmin($user){
+    public function sesionAdmin($user)
+    {
         $dbh = new Conexion;
         $conexion = $dbh->get_conexion();
-        $sql = 'Select * from admin where correo=:correo';
+        $sql = 'Select id from admin where correo=:correo';
         $stmt = $conexion->prepare($sql);
-        $stmt->bindParam(":correo",$user);
-        if(!$stmt){
+        $stmt->bindParam(":correo", $user);
+        if (!$stmt) {
             throw new Exception("Error. Hubo un fallo en la base de datos");
-        }else{
+        } else {
+            $id = $stmt->execute();
+            $this->id = $id;
+        }
+    }
+
+    public function setAdmin($user)
+    {
+        $dbh = new Conexion;
+        $conexion = $dbh->get_conexion();
+        $sql = 'Select * from admin where id=:id';
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(":id", $user);
+        if (!$stmt) {
+            throw new Exception("Error. Hubo un fallo en la base de datos");
+        } else {
             $stmt->execute();
             $datauser = $stmt->fetch(PDO::FETCH_ASSOC);
             $this->id = $datauser['id'];
             $this->nombre = $datauser['nombre'];
-            $this->apellido= $datauser['apellido'];
+            $this->apellido = $datauser['apellido'];
             $this->pass = $datauser['pass'];
-            $this->correo= $datauser['correo'];
-            $this->sexo= $datauser['sexo'];
-            $this->fechaNac= $datauser['fecha_nac'];
-            $this->titulos= $datauser['titulos'];
-            $this->estado= $datauser['estado'];
+            $this->correo = $datauser['correo'];
+            $this->sexo = $datauser['sexo'];
+            $this->fechaNac = $datauser['fecha_nac'];
+            $this->titulos = $datauser['titulos'];
+            $this->estado = $datauser['estado'];
+            $this->foto = $datauser['fotoPerfil'];
+        }
+    }
+
+    public function actuFoto($route)
+    {
+        $dbh = new Conexion;
+        $conexion = $dbh->get_conexion();
+        $sql = "update admin set fotoPerfil=:fotoPerfil where id=:id";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(":fotoPerfil", $route);
+        $stmt->bindParam(":id", $this->id);
+        if (!$stmt) {
+            throw new Exception("Error con la base de datos");
+        } else {
+            $stmt->execute();
         }
     }
 
     //funciones get
-
-    public function getId(){
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getNombreCompleto(){
-        return $this->nombre." ".$this->apellido;
+    public function getNombre()
+    {
+        return $this->nombre;
     }
 
-    public function getCorreo(){
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    public function getCorreo()
+    {
         return $this->correo;
     }
 
-    public function getSexo(){
+    public function getSexo()
+    {
         return $this->sexo;
     }
+
+    public function getFecha()
+    {
+        return $this->fechaNac;
+    }
+
+    public function getFoto()
+    {
+        return $this->foto;
+    }
 }
-?>
--- modelo/class.cliente.php
+++ modelo/class.cliente.php
@@ -1,126 +1,192 @@
 <?php
-class Cliente{
+class Cliente
+{
     private $id;
     private $nombre;
     private $apellido;
     private $pass;
     private $correo;
     private $sexo;
+    private $foto;
     private $fechaNac;
     private $id_exp;
 
-    public function veriData($nombre,$apellido,$pass,$correo,$sexo,$fechaNac){
-        if(!empty($nombre) && !empty($apellido) && !empty($pass) && !empty($correo) && isset($sexo) && !empty($fechaNac)){
+    public function veriData($nombre, $apellido, $pass, $correo, $sexo, $fechaNac)
+    {
+        if (!empty($nombre) && !empty($apellido) && !empty($pass) && !empty($correo) && isset($sexo) && !empty($fechaNac)) {
             $this->nombre = $nombre;
             $this->apellido = $apellido;
             $this->pass = $pass;
             $this->correo = $correo;
             $this->sexo = $sexo;
             $this->fechaNac = $fechaNac;
-        }else{
+        } else {
             throw new Exception('Error, Tiene que rellenar todos los datos');
         }
     }
 
     //Funcion que registra al cliente
-    public function newCliente(){
+    public function newCliente()
+    {
         $dbh = new Conexion();
         $conexion = $dbh->get_conexion();
         $sql = "insert into cliente (nombre, apellido, pass, correo, sexo, fecha_nac) values (:nombre, :apellido, md5(:pass), :correo, :sexo, :fecha_nac)";
-        $stmt = $conexion->prepare($sql) ;
-        $stmt->bindParam(":nombre",$this->nombre);
-        $stmt->bindParam(":apellido",$this->apellido);
-        $stmt->bindParam(":pass",$this->pass);
-        $stmt->bindParam(":correo",$this->correo);
-        $stmt->bindParam(":sexo",$this->sexo);
-        $stmt->bindParam(":fecha_nac",$this->fechaNac);
-        if(!$stmt){
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->bindParam(":apellido", $this->apellido);
+        $stmt->bindParam(":pass", $this->pass);
+        $stmt->bindParam(":correo", $this->correo);
+        $stmt->bindParam(":sexo", $this->sexo);
+        $stmt->bindParam(":fecha_nac", $this->fechaNac);
+        if (!$stmt) {
             throw new Exception("Error. No se pudo conectar con la base de datos");
-        }else{
+        } else {
             $stmt->execute();
             //Se a registrado correctamente
         }
     }
-    
+
     //revisa si existe un cliente con el mismo correo
-    public function extCliente($user){
+    public function extCliente($user)
+    {
         $dbh = new Conexion;
         $conexion = $dbh->get_conexion();
         $sql = 'Select * from cliente where correo=:correo';
         $stmt = $conexion->prepare($sql);
-        $stmt->bindParam(":correo",$user);
-        if(!$stmt){
+        $stmt->bindParam(":correo", $user);
+        if (!$stmt) {
             throw new Exception("Error. fallo en la base de datos");
-        }else{
+        } else {
             $stmt->execute();
-            if($stmt->rowCount()){
-                return true; 
-            }else{
+            if ($stmt->rowCount()) {
+                return true;
+            } else {
                 return false;
             }
         }
     }
 
     //busca el cliente para el inicio de sesion
-    public function searchCliente($user, $pass){
+    public function searchCliente($user, $pass)
+    {
         $dbh = new Conexion;
         $conexion = $dbh->get_conexion();
         $sql = 'Select * from cliente where correo=:correo and pass=:pass';
         $stmt = $conexion->prepare($sql);
-        $stmt->bindParam(":correo",$user);
-        $stmt->bindParam(":pass",$pass);
-        if(!$stmt){
+        $stmt->bindParam(":correo", $user);
+        $stmt->bindParam(":pass", $pass);
+        if (!$stmt) {
             throw new Exception("Error. fallo en la base de datos");
-        }else{
+        } else {
             $stmt->execute();
-            if($stmt->rowCount()){
-                return true; 
-            }else{
+            if ($stmt->rowCount()) {
+                return true;
+            } else {
                 return false;
             }
         }
     }
 
-    public function setCliente($user){
+    public function sesionCliente($user)
+    {
         $dbh = new Conexion;
         $conexion = $dbh->get_conexion();
         $sql = 'Select * from cliente where correo=:correo';
         $stmt = $conexion->prepare($sql);
-        $stmt->bindParam(":correo",$user);
-        if(!$stmt){
+        $stmt->bindParam(":correo", $user);
+        if (!$stmt) {
             throw new Exception("Error. Hubo un fallo en la base de datos");
-        }else{
+        } else {
+            $stmt->execute();
+            $datauser = $stmt->fetch(PDO::FETCH_ASSOC);
+            $this->id = $datauser['id'];
+        }
+    }
+
+    public function setCliente($user)
+    {
+        $dbh = new Conexion;
+        $conexion = $dbh->get_conexion();
+        $sql = 'Select * from cliente where id=:id';
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(":id", $user);
+        if (!$stmt) {
+            throw new Exception("Error. Hubo un fallo en la base de datos");
+        } else {
             $stmt->execute();
             $datauser = $stmt->fetch(PDO::FETCH_ASSOC);
             $this->id = $datauser['id'];
             $this->nombre = $datauser['nombre'];
-            $this->apellido= $datauser['apellido'];
+            $this->apellido = $datauser['apellido'];
             $this->pass = $datauser['pass'];
-            $this->correo= $datauser['correo'];
-            $this->sexo= $datauser['sexo'];
-            $this->fechaNac= $datauser['fecha_nac'];
+            $this->correo = $datauser['correo'];
+            $this->sexo = $datauser['sexo'];
+            $this->foto = $datauser['fotoPerfil'];
+            $this->fechaNac = $datauser['fecha_nac'];
         }
     }
 
+    /*public function veriFoto($name, $ext)
+    {
+        if (isset($name) && isset($ext)) {
+            $this->foto == '../uploads/cliente/' . $_SESSION['cliente'] . '/' . $name;
+        }
+    }*/
+
+    public function actuFoto($route)
+    {
+        $dbh = new Conexion;
+        $conexion = $dbh->get_conexion();
+        $sql = "update cliente set fotoPerfil=:fotoPerfil where id=:id";
+        $stmt = $conexion->prepare($sql);
+        $stmt->bindParam(":fotoPerfil", $route);
+        $stmt->bindParam(":id", $this->id);
+        if (!$stmt) {
+            throw new Exception("Error con la base de datos");
+        } else {
+            $stmt->execute();
+        }
+    }
+
+
     //funciones get del cliente
-    public function getId(){
+    public function getId()
+    {
         return $this->id;
     }
 
-    public function getNombre(){
+    public function getNombre()
+    {
         return $this->nombre;
     }
 
-    public function getApellido(){
+    public function getApellido()
+    {
         return $this->apellido;
     }
 
-    public function getCorreo(){
+    public function getCorreo()
+    {
         return $this->correo;
     }
 
-    public function getIdExp(){
+    public function getSexo()
+    {
+        return $this->sexo;
+    }
+
+    public function getFecha()
+    {
+        return $this->fechaNac;
+    }
+
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    public function getIdExp()
+    {
         return $this->id_exp;
     }
 }
-?>
